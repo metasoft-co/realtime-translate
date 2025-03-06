1774134ce46213d1ff2fde4fa45880cfe40cc6bd
Selçuk <selcuk.guler00@gmail.com>
2025-03-06 09:00:06 +0300

    Remove unused imports and commented-out code in TTS route

--- src/app/api/tts/route.ts
+++ src/app/api/tts/route.ts
@@ -1,7 +1,5 @@
 import elevenlabs from "@/lib/elevenlabs";
-import axios from "axios";
-import * as deepl from "deepl-node";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
 	const { text, targetLang } = await req.json();
@@ -18,20 +16,6 @@ export async function POST(req: NextRequest) {
 		chunks.push(chunk);
 	}
 	const content = Buffer.concat(chunks);
-	// const ttsReq = await axios.post(
-	// 	`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
-	// 	{
-	// 		model_id: "eleven_multilingual_v2",
-	// 		text,
-	// 	},
-	// 	{
-	// 		headers: {
-	// 			"xi-api-key": "sk_f20719d6cfd1403b214f0b487bc05e04cac906a66ee779eb",
-	// 			"Content-Type": "application/json",
-	// 		},
-	// 	}
-	// );
-	// const content = await ttsReq.data;
 
 	return new Response(content, {
 		headers: {
